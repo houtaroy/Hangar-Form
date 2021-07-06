c5090f3b4c822852222656e2cceb299c0024fb1a
Houtaroy <Houtaroy@gmail.com>
2021-07-06 18:20:26 +0800

    🐞 fix(KFormDesign): 删除部分无用参数
    删除部分无用参数

--- packages/KFormDesign/module/formItemProperties.vue
+++ packages/KFormDesign/module/formItemProperties.vue
@@ -212,7 +212,7 @@
               'slider',
               'html'
             ].includes(selectItem.type) &&
-            typeof options.defaultValue !== 'undefined'
+              typeof options.defaultValue !== 'undefined'
           "
           label="默认值"
         >
@@ -408,15 +408,15 @@
         <a-form-item
           v-if="
             typeof options.hidden !== 'undefined' ||
-            typeof options.disabled !== 'undefined' ||
-            typeof options.readonly !== 'undefined' ||
-            typeof options.clearable !== 'undefined' ||
-            typeof options.multiple !== 'undefined' ||
-            typeof options.range !== 'undefined' ||
-            typeof options.showTime !== 'undefined' ||
-            typeof options.allowHalf !== 'undefined' ||
-            typeof options.showInput !== 'undefined' ||
-            typeof options.animated !== 'undefined'
+              typeof options.disabled !== 'undefined' ||
+              typeof options.readonly !== 'undefined' ||
+              typeof options.clearable !== 'undefined' ||
+              typeof options.multiple !== 'undefined' ||
+              typeof options.range !== 'undefined' ||
+              typeof options.showTime !== 'undefined' ||
+              typeof options.allowHalf !== 'undefined' ||
+              typeof options.showInput !== 'undefined' ||
+              typeof options.animated !== 'undefined'
           "
           label="操作属性"
         >
@@ -505,7 +505,7 @@
         <a-form-item
           v-if="
             typeof selectItem.rules !== 'undefined' &&
-            selectItem.rules.length > 0
+              selectItem.rules.length > 0
           "
           label="校验"
         >
@@ -568,88 +568,83 @@
  * date 2019-11-20
  * description 表单控件属性设置组件,因为配置数据是引用关系，所以可以直接修改
  */
-import KChangeOption from '../../KChangeOption/index.vue';
-import kCheckbox from '../../KCheckbox/index.vue';
+import KChangeOption from "../../KChangeOption/index.vue";
+import kCheckbox from "../../KCheckbox/index.vue";
 
 export default {
-  name: 'formItemProperties',
+  name: "formItemProperties",
   data() {
     return {
       familyOptions: [
         // 字体选择设置
         {
-          value: 'SimSun',
-          label: '宋体'
+          value: "SimSun",
+          label: "宋体"
         },
         {
-          value: 'FangSong',
-          label: '仿宋'
+          value: "FangSong",
+          label: "仿宋"
         },
         {
-          value: 'SimHei',
-          label: '黑体'
+          value: "SimHei",
+          label: "黑体"
         },
         {
-          value: 'PingFangSC-Regular',
-          label: '苹方'
+          value: "PingFangSC-Regular",
+          label: "苹方"
         },
         {
-          value: 'KaiTi',
-          label: '楷体'
+          value: "KaiTi",
+          label: "楷体"
         },
         {
-          value: 'LiSu',
-          label: '隶书'
+          value: "LiSu",
+          label: "隶书"
         }
       ],
       sizeOptions: [
         //字号选择设置
         {
-          value: '26pt',
-          label: '一号'
+          value: "26pt",
+          label: "一号"
         },
         {
-          value: '24pt',
-          label: '小一'
+          value: "24pt",
+          label: "小一"
         },
         {
-          value: '22pt',
-          label: '二号'
+          value: "22pt",
+          label: "二号"
         },
         {
-          value: '18pt',
-          label: '小二'
+          value: "18pt",
+          label: "小二"
         },
         {
-          value: '16pt',
-          label: '三号'
+          value: "16pt",
+          label: "三号"
         },
         {
-          value: '15pt',
-          label: '小三'
+          value: "15pt",
+          label: "小三"
         },
         {
-          value: '14pt',
-          label: '四号'
+          value: "14pt",
+          label: "四号"
         },
         {
-          value: '12pt',
-          label: '小四'
+          value: "12pt",
+          label: "小四"
         },
         {
-          value: '10.5pt',
-          label: '五号'
+          value: "10.5pt",
+          label: "五号"
         },
         {
-          value: '9pt',
-          label: '小五'
+          value: "9pt",
+          label: "小五"
         }
-      ],
-      ruleRangeDefault: {
-        min: 0,
-        max: 9999,
-        message: '数据内容不符合长度范围'
-      }
+      ]
     };
   },
   computed: {
@@ -670,17 +665,6 @@ export default {
   components: {
     KChangeOption,
     kCheckbox
-  },
-  methods: {
-    changeRuleRange(value) {
-      value
-        ? this.selectItem.rules.splice(
-            1,
-            0,
-            Object.assign({}, this.ruleRangeDefault)
-          )
-        : this.selectItem.rules.splice(1, 1);
-    }
   }
 };
 </script>
