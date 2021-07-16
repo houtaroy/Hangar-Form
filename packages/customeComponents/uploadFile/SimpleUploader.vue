9ea8d5e102fc3438e8dc6a3c71bf13a785989b53
Houtaroy <houtaroy@gmail.com>
2021-07-16 00:51:53 +0000

    !9 修复自定义上传组件代码合并的问题 * 🐞 fix(HForm/SimpleUploader): 修改部分代码合并问题

--- packages/customeComponents/uploadFile/SimpleUploader.vue
+++ packages/customeComponents/uploadFile/SimpleUploader.vue
@@ -94,7 +94,6 @@
 <script>
 import axios from 'axios';
 import qs from 'qs';
-import uploader from 'vue-simple-uploader';
 // import storage from 'store';
 // import { ACCESS_TOKEN } from '@/store/mutation-types';
 export default {
