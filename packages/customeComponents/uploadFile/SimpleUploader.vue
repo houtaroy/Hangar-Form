8d73f8c6c9f0370d03d3ceef9e60ffde4ca798ea
Houtaroy <82852852+Houtaroy@users.noreply.github.com>
2021-08-31 10:18:02 +0800

    Revert "上传删除前增加确认提示"
    This reverts commit 6ad0f7cd0772c9f71d25ac012eae91c70f386156.

--- packages/customeComponents/uploadFile/SimpleUploader.vue
+++ packages/customeComponents/uploadFile/SimpleUploader.vue
@@ -190,22 +190,12 @@ export default {
      * @return {type} 无返回
      */
     delFile(index) {
-      const that = this;
-      this.$confirm({
-        title: '确定要删除吗?',
-        okText: '确定',
-        okType: 'danger',
-        cancelText: '取消',
-        onOk() {
-          that.fileData.splice(index, 1);
-          if (that.fileData.length === 0) {
-            that.$emit('input', '');
-          } else {
-            that.$emit('input', JSON.stringify(that.fileData));
-          }
-          that.$message.success('保存成功');
-        }
-      });
+      this.fileData.splice(index, 1);
+      if (this.fileData.length === 0) {
+        this.$emit('input', '');
+      } else {
+        this.$emit('input', JSON.stringify(this.fileData));
+      }
     },
     /**
      * @description 下载附件
