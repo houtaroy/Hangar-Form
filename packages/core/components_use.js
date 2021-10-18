9da5fccc9d56a2c8db98f57aecd4377bc967b5f1
mysmiles <1490767581@qq.com>
2021-10-18 11:21:17 +0800

    新增office组件

--- packages/core/components_use.js
+++ packages/core/components_use.js
@@ -61,6 +61,7 @@ import uploader from 'vue-simple-uploader';
 import HCommentOptions from '../customeComponents/commonOptions/commonOptions';
 import HDatePicker from '../customeComponents/HDatePicker/HDatePicker';
 import HAceEditor from '../customeComponents/ace-editor';
+import HWebOffice from '../customeComponents/webOffice/webOffice';
 
 export default {
   input: Input,
@@ -127,6 +128,7 @@ Vue.component('h-date-picker', HDatePicker);
 Vue.component('h-uploader', HUploader);
 Vue.use(uploader);
 Vue.use(HAceEditor);
+Vue.use(HWebOffice);
 
 Vue.prototype.$confirm = Modal.confirm;
 Vue.prototype.$message = message;
